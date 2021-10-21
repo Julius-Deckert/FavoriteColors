68de9898b99f2c3c520f220d7fc13eb239768274
Julius <julius.deckert@gmail.com>
2021-10-21 19:49:21 +0200

    Add color enum

---
+++ FavoriteColors/Domain/Models/ColorEnum.cs
@@ -0,0 +1,13 @@
+﻿namespace FavoriteColors.Domain.Models
+{
+    public enum ColorEnum
+    {
+        blau = 1,
+        grün = 2,
+        violett = 3,
+        rot = 4,
+        gelb = 5,
+        türkis = 6,
+        weiß = 7
+    }
+}
