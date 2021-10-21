6fd370a9de1284ac2acff89762d567090a788eba
Julius <julius.deckert@gmail.com>
2021-10-21 19:49:10 +0200

    Add color model and service

---
+++ FavoriteColors/Domain/Models/Color.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace FavoriteColors.Domain.Models
+{
+    public class Color
+    {
+        public Color()
+        {
+        }
+
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
