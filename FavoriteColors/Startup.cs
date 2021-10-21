3781e8fa94a7387ffb353cdf52934af9c8fc9e86
Julius <julius.deckert@gmail.com>
2021-10-21 19:51:33 +0200

    Adjust Main and StartUp

--- FavoriteColors/Startup.cs
+++ FavoriteColors/Startup.cs
@@ -1,42 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using FavoriteColors.Domain.Repositories;
+using FavoriteColors.Domain.Services;
+using FavoriteColors.Persistence.Contexts;
+using FavoriteColors.Persistence.Repositories;
+using FavoriteColors.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace FavoriteColors
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddDbContext<AppDbContext>(options => {
+                options.UseInMemoryDatabase("favoriecolors-in-memory");
+            });
+
+            services.AddScoped<IPersonRepository, PersonRespository>();
+            services.AddScoped<IPersonService, PersonService>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.UseHttpsRedirection();
 
             app.UseRouting();
 
