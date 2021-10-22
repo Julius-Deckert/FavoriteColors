f34bbf915503199a0fc3fc63a0f0097f69df1ebd
Julius <julius.deckert@gmail.com>
2021-10-22 12:21:14 +0200

    Add person model for POST operation

---
+++ FavoriteColors/Resources/SavePersonResource.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using FavoriteColors.Domain.Models;
+
+namespace FavoriteColors.Resources
+{
+    public class SavePersonResource
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        [MaxLength(5)]
+        public int ZipCode { get; set; }
+
+        [Required]
+        public string City { get; set; }
+
+        [Required]
+        [EnumDataType(typeof(ColorEnum))]
+        public string Color { get; set; }
+    }
+}
