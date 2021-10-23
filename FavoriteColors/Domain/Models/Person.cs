1c18cb0d3aaf9c1c11782096dc7c32e8a9cec075
Julius-Deckert <julius.deckert@gmail.com>
2021-10-23 14:27:27 +0200

    Remove ColorId property from Person and add indices to properties

--- FavoriteColors/Domain/Models/Person.cs
+++ FavoriteColors/Domain/Models/Person.cs
@@ -1,26 +1,23 @@
-﻿using System.Collections.Generic;
+﻿using CsvHelper.Configuration.Attributes;
 
 namespace FavoriteColors.Domain.Models
 {
     public class Person
     {
-        public Person()
-        {
-        }
-
         public int Id { get; set; }
 
+        [Index(0)]
         public string Name { get; set; }
 
+        [Index(1)]
         public string LastName { get; set; }
 
+        [Index(2)]
         public int ZipCode { get; set; }
 
         public string City { get; set; }
 
-
-        public int ColorId { get; set; }
-
+        [Index(3)]
         public string Color { get; set; }
     }
 }
