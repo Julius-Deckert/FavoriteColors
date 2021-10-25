4cbe9c9f07a4d5d3708dc9346d857cd96816e0b9
Julius-Deckert <julius.deckert@gmail.com>
2021-10-25 10:37:10 +0200

    Add unit test project to solution

---
+++ FavoriteColors.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace FavoriteColors.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
