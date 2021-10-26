77b7d8cc2a0c99d5e8f495c6049d1d5fe39d5e1d
Julius-Deckert <julius.deckert@gmail.com>
2021-10-27 01:33:25 +0200

    Revise and add unit tests for PersonsController

--- FavoriteColors.UnitTests/PersonsControllerTests.cs
+++ FavoriteColors.UnitTests/PersonsControllerTests.cs
@@ -1,11 +1,11 @@
+using System;
 using System.Threading.Tasks;
 using FavoriteColors.Controllers;
 using FavoriteColors.Domain.Models;
-using FavoriteColors.Persistence.Contexts;
+using FavoriteColors.Dtos;
 using FavoriteColors.Persistence.Repositories;
-using FavoriteColors.Services;
+using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Moq;
 using Xunit;
 
@@ -13,53 +13,112 @@ namespace FavoriteColors.UnitTests
 {
     public class PersonsControllerTests
     {
-        private readonly Mock<DbSet<Person>> setMock = new();
-        private readonly Mock<AppDbContext> contextMock = new();
+        private readonly Random rand = new();
+        private readonly Array colors = Enum.GetValues(typeof(Color));
+        private readonly Mock<PersonRepository> repoMock = new();
 
         [Fact]
-        public async Task GetByIdAsync_WithUnexistingId_ReturnsNotFound()
+        public async Task GetItemAsync_WithUnexistingItem_ReturnsNotFound()
         {
             // Arrange
-            contextMock.Setup(m => m.Persons).Returns(setMock.Object);
-            var repoMock = new Mock<PersonRepository>(contextMock.Object);
-            var personServiceMock = new Mock<PersonService>(repoMock.Object);
-            personServiceMock.Setup(service => service.GetByIdAsync(It.IsAny<int>())).ReturnsAsync((Person)null);
+            repoMock.Setup(repo => repo.GetByIdAsync(It.IsAny<int>()))
+                .ReturnsAsync((Person)null);
 
-            var controller = new PersonsController(personServiceMock.Object);
+            var controller = new PersonsController(repoMock.Object);
 
             // Act
-            var result = await controller.GetByIdAsync(0);
+            var result = await controller.GetByIdAsync(rand.Next());
 
             // Assert
-            Assert.IsType<NotFoundResult>(result.Result);
+            result.Result.Should().BeOfType<NotFoundResult>();
         }
 
-        //[Fact]
-        //public async Task GetByIdAsync_WithExistingId_ReturnsExpectedPerson()
-        //{
-        //    // Arrange
-        //    var expectedPerson = CreateTestPerson();
+        [Fact]
+        public async Task GetItemAsync_WithExistingItem_ReturnsExpectedItem()
+        {
+            // Arrange
+            var expectedItem = CreateRandomPerson();
 
-        //    var repoMock = new Mock<PersonRepository>(contextMock.Object);
-        //    var personServiceMock = new Mock<PersonService>(repoMock.Object);
-        //    personServiceMock.Setup(service => service.GetByIdAsync(It.IsAny<int>())).ReturnsAsync((Person)null);
+            repoMock.Setup(repo => repo.GetByIdAsync(It.IsAny<int>()))
+                .ReturnsAsync(expectedItem);
 
-        //    // Act
+            var controller = new PersonsController(repoMock.Object);
 
-        //    // Assert
-        //}
+            // Act
+            var result = await controller.GetByIdAsync(expectedItem.Id);
 
-        private Person CreateTestPerson()
+            // Assert
+            result.Value.Should().BeEquivalentTo(expectedItem);
+        }
+
+        [Fact]
+        public async Task GetItemsAsync_WithExistingItems_ReturnsAllItems()
+        {
+            // Arrange
+            var expectedItems = new[] { CreateRandomPerson(), CreateRandomPerson(), CreateRandomPerson() };
+
+            repoMock.Setup(repo => repo.GetAllAsync())
+                .ReturnsAsync(expectedItems);
+
+            var controller = new PersonsController(repoMock.Object);
+
+            // Act
+            var actualItems = await controller.GetAllAsync();
+
+            // Assert
+            actualItems.Should().BeEquivalentTo(expectedItems);
+        }
+
+        [Fact]
+        public async Task CreateItemAsync_WithItemToCreate_ReturnsCreatedItem()
+        {
+            // Arrange
+            var itemToCreate = new CreatePersonDto(
+                rand.Next(),
+                Guid.NewGuid().ToString(),
+                Guid.NewGuid().ToString(),
+                rand.Next(),
+                Guid.NewGuid().ToString(),
+                Guid.NewGuid().ToString());
+
+            var controller = new PersonsController(repoMock.Object);
+
+            // Act
+            var result = await controller.CreatePersonAsync(itemToCreate);
+
+            // Assert
+            var createdItem = (result.Result as CreatedAtActionResult).Value as PersonDto;
+            itemToCreate.Should().BeEquivalentTo(
+                createdItem,
+                options => options.ComparingByMembers<PersonDto>().ExcludingMissingMembers()
+            );
+        }
+
+        private Person CreateRandomPerson()
         {
             return new()
             {
-                Id = 1,
-                Name = "Max",
-                LastName = "Mustermann",
+                Id = rand.Next(),
+                Name = GenerateRandomString(8),
+                LastName = GenerateRandomString(8),
                 ZipCode = 12345,
-                City = "Musterstadt",
-                Color = Color.blau.ToString()
+                City = GenerateRandomString(8),
+                Color = colors.GetValue(rand.Next(colors.Length)).ToString()
             };
+        }
+
+        private string GenerateRandomString(int length)
+        {
+            var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz ";
+            var Charsarr = new char[length];
+            var random = new Random();
+
+            for (int i = 0; i < Charsarr.Length; i++)
+            {
+                Charsarr[i] = characters[random.Next(characters.Length)];
+            }
+
+            return new string(Charsarr);
         }
     }
 }
